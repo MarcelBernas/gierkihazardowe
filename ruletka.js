2ed5611724d625cdf3403e5b53f2b3516fbe5e31
MarcelBernas <marcelbernas59@gmail.com>
2024-11-30 14:30:37 +0100

    modified:   index.html 	modified:   ruletka.html 	modified:   ruletka.js 	new file:   user.png

--- ruletka.js
+++ ruletka.js
@@ -4,6 +4,41 @@ let activeElement;
 let returnTopValue;
 let returnLeftValue;
 
+if (document.cookie.split("; ").find((row) => row.startsWith("username="))?.split("=")[1] === undefined) {
+    document.cookie = "username=" + window.prompt('Witaj w strefie hazardu! Proszę podać nazwę użytkownika.') + "; SameSite=None; secure; expires=Fri, 20 Aug 2077 12:00:00 UTC; path=/";
+  }
+  if (document.cookie.split("; ").find((row) => row.startsWith("chips="))?.split("=")[1] === undefined) {
+    document.cookie = "chips=1000; SameSite=None; secure; expires=Fri, 20 Aug 2077 12:00:00 UTC; path=/";
+  }
+  function updateUserInfo() {
+    document.getElementById('user-name').innerText = document.cookie.split("; ").find((row) => row.startsWith("username="))?.split("=")[1];
+    document.getElementById('token_count').innerText = 'Masz ' + document.cookie.split("; ").find((row) => row.startsWith("chips="))?.split("=")[1] + ' żetonów.';
+  }
+  function redirectToHome() {
+    window.location.href = 'index.html';
+  }
+  function editUserName() {
+    const userNameSpan = document.getElementById('user-name');
+    const editNameTextarea = document.getElementById('edit-name');
+
+    userNameSpan.style.display = 'none';
+    editNameTextarea.style.display = 'block';
+    editNameTextarea.value = userNameSpan.innerText;
+    editNameTextarea.focus();
+
+    editNameTextarea.addEventListener('keypress', function (e) {
+      if (e.key === 'Enter') {
+        e.preventDefault();
+
+
+        userNameSpan.innerText = editNameTextarea.value.trim() || 'nazwa użytkownika';
+        userNameSpan.style.display = 'block';
+        editNameTextarea.style.display = 'none';
+        document.cookie = "username=" + editNameTextarea.value + "; SameSite=None; secure; expires=Fri, 20 Aug 2077 12:00:00 UTC; path=/";
+      }
+    });
+  }
+
 function addListener(element) {
     element.addEventListener('mousedown', mouseDown);
 }
@@ -73,7 +108,7 @@ function updateBet(chip, betName) {
         }
         totalbet += chip;
         localTokens -= chip;
-        document.getElementById("chips_count").innerText = "Masz " + localTokens + " żetonów. Obecna suma zakładów: " + totalbet + ".";
+        document.getElementById("current_bet").innerText = "Obecna suma zakładów: " + totalbet + ".";
     }
 }
 
@@ -85,7 +120,7 @@ function resetBet() {
         localTokens = document.cookie.split("; ").find((row) => row.startsWith("chips="))?.split("=")[1];
         totalbet = 0;
         bets = [];
-        document.getElementById("chips_count").innerText = "Masz " + localTokens + " żetonów. Obecna suma zakładów: " + totalbet + ".";
+        document.getElementById("current_bet").innerText = "Obecna suma zakładów: " + totalbet + ".";
     }
 
 }
@@ -169,9 +204,9 @@ function startRoulette() {
             resetBet();
             rouletteStrip.removeEventListener('transitionend', resetPosition); // Usunięcie event listenera
             if (document.cookie.split("; ").find((row) => row.startsWith("chips="))?.split("=")[1] < 1) {
-                window.alert("Skończyły się Tobie żetony. Wciśnij 'OK', żeby zresetować swoje postępy.");
+                window.alert("Nie masz już żadnych żetonów. Wciśnij 'OK', żeby zresetować swoje postępy.");
                 document.cookie = "chips=1000; SameSite=None; secure; expires=Fri, 20 Aug 2077 12:00:00 UTC; path=/";
-                window.location.replace("/");
+                location.reload(); 
             }
         });
     }
